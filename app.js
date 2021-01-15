925b88f1aca821b368a8db9d7006e31a9f2a5d3f
vinit-churi <vinitchuri@hotmail.com>
2021-01-15 19:21:34 +0530

    added few more features

--- app.js
+++ app.js
@@ -2,15 +2,19 @@ document.addEventListener("DOMContentLoaded", () => {
   const grid = document.querySelector(".grid");
   const doodler = document.createElement("div");
   let doodlerLeftSpace = 50;
-  let doodlerBottomSpace = 250;
+  let startPoint = 150;
+  let doodlerBottomSpace = startPoint;
   const platformCount = 5;
   let isGameOver = false;
   let platforms = [];
   let upTimeId;
   let downTimeId;
+  isJumping = true;
   function createDoodler() {
     grid.appendChild(doodler);
     doodler.classList.add("doodler");
+    doodlerLeftSpace = platforms[0].left;
+    // doodlerBottomSpace = platforms[0].bottom;
     doodler.style.left = doodlerLeftSpace + "px";
     doodler.style.bottom = doodlerBottomSpace + "px";
   }
@@ -52,34 +56,59 @@ document.addEventListener("DOMContentLoaded", () => {
 
   function fall() {
     clearInterval(upTimeId);
+    isJumping = false;
     downTimeId = setInterval(function () {
       doodlerBottomSpace -= 5;
       doodler.style.bottom = doodlerBottomSpace + "px";
       if (doodlerBottomSpace <= 0) {
         gameOver();
       }
-    });
+
+      platforms.forEach((platform) => {
+        if (
+          doodlerBottomSpace >= platform.bottom &&
+          doodlerBottomSpace <= platform.bottom + 15 &&
+          doodlerLeftSpace + 60 >= platform.left &&
+          doodlerLeftSpace <= platform.left + 85 &&
+          !isJumping
+        ) {
+          console.log("landed");
+          startPoint = doodlerBottomSpace;
+          jump();
+        }
+      });
+    }, 30);
   }
 
   function gameOver() {
     console.log("gameOver");
+    isGameOver = true;
+    clearInterval(upTimeId);
+    clearInterval(downTimeId);
   }
 
   function jump() {
     clearInterval(downTimeId);
+    isJumping = true;
     upTimeId = setInterval(function () {
       doodlerBottomSpace += 20;
       doodler.style.bottom = doodlerBottomSpace + "px";
-      if (doodlerBottomSpace > 350) {
+      if (doodlerBottomSpace > startPoint + 200) {
         fall();
       }
     }, 30);
   }
 
+  function control(e) {
+    if (e.key === "ArrowLeft") {
+      // move left
+    }
+  }
+
   function start() {
     if (!isGameOver) {
-      createDoodler();
       createPlatform();
+      createDoodler();
       setInterval(movePlatforms, 30);
       jump();
     }
